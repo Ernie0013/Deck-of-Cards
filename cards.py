7eecbd6b1c92445a34e3296a8f4890d598a8a1f8
Ernie Collins <ernie4789@gmail.com>
2021-01-22 16:48:05 +0000

    Now using emoji for cards

--- cards.py
+++ cards.py
@@ -1,7 +1,14 @@
 import random
+from discord import Client
+
+client = Client()
 
 cardSuits = ('Spades', 'Diamonds', 'Hearts', 'Clubs')
 cardValues = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+
+suitEmojis = ('<:bspades:802184587395465237>', '<:rdiamonds:802184587521556521>','<:rhearts:802184587514085397>', '<:bclubs:802184587702042656>')
+valueEmojisRed = ('<:ra:802184587757223966>', '<:r2:802184587660886093>', '<:r3:802184587979391006>', '<:r4:802184587974410260>', '<:r5:802184587715018782>', '<:r6:802184587672813619>', '<:r7:802184587718688818>', '<:r8:802184587798511677>', '<:r9:802184587710562305>', '<:r10:802184587845304340>', '<:rj:802184587760369684>', '<:rq:802184587971133531>', '<:rk:802184587647385681>')
+valueEmojisBlack = ('<:ba:802184587563499531>', '<:b2:802184587530862604>', '<:b3:802184587182080031>', '<:b4:802184587316035595>', '<:b5:802184587605180456>', '<:b6:802184587593777162>', '<:b7:802184587589320715>', '<:b8:802184587572019250>', '<:b9:802184587593252886>', '<:b10:802184587186536499>', '<:bj:802184587680809000>', '<:bq:802184587668357170>', '<:bk:802184587663900702>')
 
 class Card:
   def __init__(self, suit: cardSuits, value: cardValues):
@@ -17,11 +24,20 @@ class Card:
     return self.suit == eq.suit and self.value == eq.value
 
   def __str__(self):
-    return self.value + ' of ' + self.suit
+    suitIndex = cardSuits.index(self.suit)
+    valueIndex = cardValues.index(self.value)
+
+    suitEmoji = suitEmojis[suitIndex]
+    valueEmoji = valueEmojisBlack[valueIndex] if suitIndex == 0 or suitIndex == 3 else valueEmojisRed[valueIndex]
+
+    return self.value + '\tof\t' + self.suit + '\n' + valueEmoji + '\n' + suitEmoji
 
 class Deck:
   def __init__(self):
     self.cards = []
+    self.add_deck()
+
+  def add_deck(self):
     for suit in cardSuits:
       for value in cardValues:
         self.cards.append(Card(suit, value))
--- main.py
+++ main.py
@@ -1,54 +1,140 @@
 import os
+import re
 from discord import Client
+from discord import Embed 
 from discord import Message
+from discord import DMChannel
+from discord import Guild
+from discord import Member
 from discord import TextChannel
 from discord import User
-import requests
-import json
 from replit import db
 from cards import Deck
+from keep_alive import keep_alive
 
 client = Client()
 deck = Deck()
+dealt_hands = {}
+prefix = '$'
 
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+def get_hand_embed(user: User, hand: []):
+  cardDict = {
+    "title": user.display_name + "'s Hand",
+    "type": "rich",
+    "description": get_hand_string(hand),
+    "color": 16711680 if hand is None or len(hand) == 0 else 32768
+  }
+  return Embed.from_dict(cardDict)
 
-async def deal_hand(user: User):
+def get_user_hand(user: User):
+  global dealt_hands
+  if user.id in dealt_hands:
+    return dealt_hands[user.id]
+  else:
+    return None
+
+def get_hand_string(hand: []):
+  handString = ''
+  if hand is None or len(hand) == 0:
+    handString = 'No cards in hand'
+  else:
+    for card in hand:
+      handString = handString + str(card) + '\n'
+  return handString
+
+async def send_hand_to_user(user: User):
+  await user.send(embed = get_hand_embed(user, get_user_hand(user)))
+
+def deal_hand(user: User, numberOfCards: int):
   hand = []
-  hand.append(deck.take_random_card_from_deck())
-  hand.append(deck.take_random_card_from_deck())
-  hand.append(deck.take_random_card_from_deck())
-  await user.send('Your hand: ' + str(hand[0]) + ' & ' + str(hand[1]) + ' & ' + str(hand[2]))
+  for i in range(numberOfCards):
+    card = deck.take_random_card_from_deck()
+    hand.append(card)
+  dealt_hands[user.id] = hand
+
+def get_dealer_role(guild: Guild):
+  return guild.get_role(db['dealer_role'])
+
+def check_dealer_role(member: Member):
+  return get_dealer_role(member.guild) in member.roles
 
 async def send_message(message: Message):
   global deck
+  global dealt_hands
+  global prefix
   if message.author == client.user:
     return
 
-  if message.content.startswith('$deal_hand') and isinstance(message.channel, TextChannel):
+  if message.content.startswith(prefix + 'prefix') and message.author.guild_permissions.administrator and isinstance(message.channel, TextChannel):
+    prefixText = re.search(rf'{re.escape(prefix)}prefix (\S+)', message.content)
+    if prefixText is not None and prefixText.group(1) is not None:
+      prefix = prefixText.group(1)
+      await message.channel.send('Prefix has been changed to `' + prefix + '`')
+    else:
+      await message.channel.send('Prefix is `' + prefix + '`')
+
+  if message.content.startswith(prefix + 'dealer-role') and isinstance(message.channel, TextChannel):
+    if message.role_mentions is not None and len(message.role_mentions) > 0:
+      if message.author.guild_permissions.administrator:
+        db['dealer_role'] = message.role_mentions[0].id
+      else:
+        await message.channel.send('Administrator privledges required to change dealer role')
+    await message.channel.send('**' + str(get_dealer_role(message.guild)) + '** role is the dealer role')
+
+  if message.content.startswith(prefix + 'deal-hand') and isinstance(message.channel, TextChannel): 
+    if not check_dealer_role(message.author):
+      await send_error_embed(message.channel, 'You are not the dealer')
+      return
     store_channel(message.channel)
-    await deal_hand(message.author)
+    numberText = re.search(rf'{re.escape(prefix)}deal-hand (\d+)', message.content)
+    number = 2
+    if numberText is not None and numberText.group(1) is not None:
+      number = int(numberText.group(1))
+    if number > deck.number_of_cards_remaining():
+      await send_error_embed(message.channel, 'Not enough cards left in deck to deal hand')
+      return
+    deal_hand(message.author, number)
+    await send_hand_to_user(message.author)
     await message.channel.send('Dealt you a hand. Check your DM. Number of cards remaining in deck: ' + str(deck.number_of_cards_remaining()))
-  elif message.content.startswith('$deal_hand') and not isinstance(message.channel, TextChannel):
+  elif message.content.startswith(prefix + 'deal-hand') and not isinstance(message.channel, TextChannel):
     await message.channel.send('Cannot deal hand from DM.')
 
-  if message.content.startswith('$reshuffle_deck') and isinstance(message.channel, TextChannel):
-    deck = Deck()
-    await message.channel.send('Deck has been reshuffled')
+  if message.content.startswith(prefix + 'add-deck') and isinstance(message.channel, TextChannel):
+    if not check_dealer_role(message.author):
+      await send_error_embed(message.channel, 'You are not the dealer')
+      return
+    deck.add_deck()
+    await message.channel.send('Deck added. Number of cards in deck: ' + str(deck.number_of_cards_remaining()))
 
-  if message.content.startswith('$inspire'):
-    quote = get_quote()
-    await message.author.send(quote)
+  if message.content.startswith(prefix + 'show-hand') and isinstance(message.channel, DMChannel):
+    returnChannel = retrieve_channel()
+    await returnChannel.send(embed = get_hand_embed(message.author, get_user_hand(message.author)))
+    await message.channel.send('Your hand has been shown in ' + message.channel.name)
+  elif message.content.startswith(prefix + 'show-hand') and isinstance(message.channel, TextChannel):
+    await message.channel.send(embed = get_hand_embed(message.author, get_user_hand(message.author)))
+
+  if (message.content.startswith(prefix + 'shuffle') or message.content.startswith(prefix + 'reshuffle')) and isinstance(message.channel, TextChannel):
+    if not check_dealer_role(message.author):
+      await send_error_embed(message.channel, 'You are not the dealer')
+      return
+    deck = Deck()
+    dealt_hands = {}
+    await message.channel.send('Deck has been reshuffled. Number of cards in deck: ' + str(deck.number_of_cards_remaining()))
 
 def store_channel(channel: TextChannel):
   db["channel"] = channel.id
 
 def retrieve_channel():
   return client.get_channel(db["channel"])
+
+async def send_error_embed(channel: TextChannel, errorMessage: str):
+  embedDict = {
+    "title": "Error!",
+    "type": "rich",
+    "description": errorMessage,
+    "color": 16711680
+  }
+  await channel.send(embed = Embed.from_dict(embedDict))
 
 @client.event
 async def on_ready():
@@ -63,4 +149,5 @@ async def on_message_edit(before: Message, after: Message):
   if before.content != after.content:
     await send_message(after)
 
+keep_alive()
 client.run(os.getenv('TOKEN'))
